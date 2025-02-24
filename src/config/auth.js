d92d7a5cd645a59fa536b5a7f0f2a37d3880bae8
nikslogy <nikitpotdar@gmail.com>
2025-02-24 10:07:57 +0530

    increased the login time

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     // Session duration in minutes
-    // SESSION_DURATION: process.env.NEXT_PUBLIC_SESSION_DURATION || 20,
-    SESSION_DURATION: 1,
+    SESSION_DURATION: process.env.NEXT_PUBLIC_SESSION_DURATION || 20,
+    // SESSION_DURATION: 1,
     // Add other auth-related configuration here
   }
